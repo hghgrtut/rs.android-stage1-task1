1a3ba720fb2a31e6bfbd443b6377a061c48004ec
Hg Hg <hghghghg.hh39@gmail.com>
2020-03-22 21:48:33 +0300

    feat: finish 1, 2 tasks

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -1,9 +1,30 @@
 package subtask1
 
+
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var n = sadArray.size
+        var flag=1
+        var i=1
+        while (flag==1){
+            flag=0
+            for (i in 1 until n-1){
+                while (i+1<n && sadArray[i] > sadArray[i - 1] + sadArray[i + 1]) {
+                    flag=1
+                    for (j in i + 1 until n) {
+                        val k = sadArray[j]
+                        sadArray[j] = sadArray[j - 1]
+                        sadArray[j - 1] = k
+                    }
+                    n--
+                }
+            }
+        }
+        var happyArray = IntArray(n)
+        for (i in 0 until n) {
+            happyArray[i] = sadArray[i]
+        }
+        return happyArray
     }
 }
--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -2,8 +2,18 @@ package subtask2
 
 class MiniMaxSum {
 
-    // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var sum=0
+        var min=input[0]
+        var max=input[0]
+        for (i in input){
+            sum+=i
+            if (i<min) min=i
+            else if (i>max) max=i
+        }
+        var output = IntArray(2)
+        output[0]=sum-max
+        output[1]=sum-min
+        return output
     }
 }
